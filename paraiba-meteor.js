5d0f02361b83ed6f98dee4119edee90f5845d58c
Jônatas Paganini <jonatasdp@gmail.com>
2013-04-11 17:54:30 -0300

    adicionado coleções a parte de forma sustentável

--- paraiba-meteor.js
+++ paraiba-meteor.js
@@ -1,7 +1,10 @@
 Cards = new Meteor.Collection("cards");
+Phones = new Meteor.Collection("phones");
+Emails = new Meteor.Collection("emails");
+Sites = new Meteor.Collection("sites");
 Mouses = new Meteor.Collection("mouses");
 function get(id){
-  return document.getElementById(id).value;
+  return (elem = document.getElementById(id)) && elem.value || "";
 }
 function now() {
   return new Date().getTime();
@@ -9,6 +12,7 @@ function now() {
 function randomColor(){
   return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
+myCard = {};
 if (Meteor.isClient) {
   Meteor.subscribe('cards');
   Meteor.subscribe('mouses');
@@ -17,22 +21,74 @@ if (Meteor.isClient) {
      Session.set("mouse", Mouses.insert({user: Session.get("user"), color: randomColor()}));
   });
   Template.cards.cards = function () {
-    return Cards.find({},{sort: {first_at: -1}});
+    return Cards.find({$or: [{editing: null},{ _id: Session.get("myCard")}]},{sort: {first_at: -1}});
   };
   Template.mouses.mouses = function () {
     return Mouses.find();
   };
+  Meteor.startup(function(){
+    if (Session.get("myCard"))
+      return ;
+    else
+      Session.set("myCard", 
+        Cards.insert({
+          description: get("description"),
+          name: get("name"),
+          editing: true,
+          first_at: now() + 1000000
+      }));
+  });
 
+  Template.card.emails = function(){
+    return Emails.find({card_id:this._id});
+  };
+  Template.card.phones = function(){
+    return Phones.find({card_id:this._id});
+  };
+  Template.card.sites= function(){
+    return Sites.find({card_id:this._id});
+  };
+  Template.yourcard.phones = function(){
+    return Phones.find({card_id: Session.get("myCard")});
+  }
+  Template.yourcard.sites = function(){
+    return Sites.find({card_id: Session.get("myCard")});
+  }
+  Template.yourcard.emails = function(){
+    return Emails.find({card_id: Session.get("myCard")});
+  }
   Template.yourcard.events({
+    "keyup input" : function (evt) {
+      value = get(evt.target.id);
+      update = {};
+      update[evt.target.id] = value;
+      Cards.update(Session.get("myCard"), {$set: update});
+    },
     'click input.add' : function (evt) {
-      Cards.insert({
+      Cards.update(this._id,
+        {$set: {
         description: get("description"),
         name: get("name"),
-        email: get("email"),
-        site: get("site"),
-        phone: get("phone"),
-        first_at: now()
+        first_at: now(),
+        editing: null
+       }
       });
+    },
+    'click input.add_other' : function (evt) {
+      collection = eval($(evt.target).attr('add_to'));
+      data = { card_id : Session.get("myCard") };
+      console.log( 'SELECTOR',$(evt.target).parent().find("input:not([type=button]), select"));
+      $(evt.target).parent().find("input:not([type=button]), select").each(function(i,element){
+        data[element.id ] = element.value;
+        if (element.type == "text")
+          element.value = "";
+      });
+      console.log(data);
+      collection.insert( data);
+    },
+    'click a.delete': function(evt){
+      collection = eval($(evt.target).attr('from'));
+      collection.remove(this._id);
     }
   });
   Template.card.events({
